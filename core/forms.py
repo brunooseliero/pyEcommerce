2ae33e51065d72944bd2118d6d7824d176154842
Bruno Oseliero de Souza Giampietro <brunooseliero@gmail.com>
2017-05-01 19:33:16 -0300

    criando  a classe para o formulario de contato.
    criando  a classe para o formulário de contato e colocando uma pasta no
    gitignore.

---
+++ core/forms.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+from django import forms
+
+class ContactForm(forms.Form):
+    """
+    Criação de formulários no Django.
+
+    o Django tem um sistema de formulários bem diferente do que eu estou acostumado, você vai setar
+    os tipos de campos direto no código, e colocar os parâmetros.
+    Nada mais e do que uma classe onde os atributos sao os campos dos formulários,
+    e a classe tem que herdar de forms.Form que e a classe relacionada ao
+    sistema de formulários do Django.
+    """
+
+
+    name = forms.CharField(label='Nome')
+    email = forms.EmailField(label='E-mail')
+    message = forms.CharField(label='Mensagem', widget=forms.Textarea)
