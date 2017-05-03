07114326507be06c6641d27160768b0273ad16bf
Bruno Oseliero de Souza Giampietro <brunooseliero@gmail.com>
2017-05-03 09:25:09 -0300

    Comentando as linhas do código
    Comentando as linhas do código para um melhor entendimento.

--- core/views.py
+++ core/views.py
@@ -7,20 +7,25 @@ from .forms import ContactForm
 
 
 def index(request):
+    #Definição da pagina Index
+    #Rretornando a request e o template index.html.
     return render(request, 'index.html')
 
 def contact(request):
+    #Metodo para inicializar a pagina de contato
+    #Se for um metodo Post, o objeto form reebe as informações que o usuário digitou. 
     if request.method == 'POST':
         form = ContactForm(request.POST)
+        # se não, ele cria o form, passando um objeto form para o contexto da pagina html.
     else:
         form = ContactForm()
         context = {
 
             'form':form
 
-        }
+        }       
     return render(request, 'contact.html', context)
-
+    
     #retirando essa view, pois ela foi para a aplicacao certa de catalogo,
     # onde vai mostrar os produtos.
 
