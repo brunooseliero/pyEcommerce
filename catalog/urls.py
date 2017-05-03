aa234c84e419b8335b8653fca34ed8bd3d70fe3f
Bruno Oseliero de Souza Giampietro <brunooseliero@gmail.com>
2017-05-03 09:38:24 -0300

    Comentando o arquivo das URLs.

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+    #Url base, que vai trazer todos os produtos. 
     url(r'^$', views.product_list,name='product_list'),
+    #Url para epsquisar todos os produtos em cada categoria.
     url(r'^(?P<slug>[\w_-]+)/$', views.category,name='category'),
+    #Url que vai mostrar somente um produto. 
     url(r'^produto/(?P<slug>[\w_-]+)/$', views.product,name='product'),
 
 
