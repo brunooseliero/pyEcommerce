21f50b89b94b78647f95ebfafb55e6a87ec7b3d8
Bruno Oseliero de Souza Giampietro <brunooseliero@gmail.com>
2017-05-29 18:30:09 -0300

    add comentários na aplicação accounts  no arquivo views.py

--- accounts/views.py
+++ accounts/views.py
@@ -10,33 +10,47 @@ from django.contrib.auth.forms import PasswordChangeForm
 from .models import User
 
 class RegisterView(CreateView):
-
+    
+    # mdelo a ser usada
     model = User
+    # template .html a ser usado
     template_name = 'accounts/register.html'
+    # tipo qual form vai ser usado
     form_class = UserAdminCreationForm
+    # url de sucesso, se o registro der certo
     success_url = reverse_lazy('index')
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
-
+    
+    # renderização da pagina index na aplicação de contas
     template_name = 'accounts/index.html'
 
 class UpdateUserView(LoginRequiredMixin,UpdateView):
-
+    
+    # modelo a ser usada
     model = User
+    # tempalte .html a ser usado
     template_name = 'accounts/update_user.html'
+    # definição de campos que podem ser editados
     fields = ['name', 'email']
+    # url de sucesso, se o registro der certo
     success_url = reverse_lazy('accounts:index')
 
     def get_object(self):
-
+        
+        # metodo para retornar o usuário
         return self.request.user
 
 class UpdatePasswordView(LoginRequiredMixin, FormView):
-
-
+    """
+     Metodo para alteração de senha.
+    """
+    # template que vai ser usado
     template_name = 'accounts/update_password.html'
+    # url de sucesso, se o registro der certo
     success_url = reverse_lazy('accounts:index')
+    # tipo de formulário
     form_class = PasswordChangeForm
 
     def get_form_kwargs(self):
